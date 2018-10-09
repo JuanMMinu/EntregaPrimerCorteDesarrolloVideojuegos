32c6d147bb4b0d2068f2603aaab1f8b694a5e9fb
Juan Minu <arcangeljuan1@unicauca.edu.co>
2018-10-09 07:50:16 -0500

    Proyecto Escena Estatica Mushroom Mix-Up
    Entrega de primer corte de la electiva Desarrollo de Videojuegos.

---
+++ New Unity Project/Assets/Scripts/HoverMotor.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverMotor : MonoBehaviour
+{
+
+
+    public float speed = 90f;
+    public float turnSpeed = 5f;
+    public float hoverForce = 65f;
+    public float hoverHeight = 3.5f;
+
+    private float powerInput;
+    private float turnInput;
+    private Rigidbody carRigidbody;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        carRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        powerInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+    void FixedUpdate()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, hoverHeight))
+        {
+            float proportionalHeight = (hoverHeight = hit.distance) / hoverHeight;
+            Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverHeight;
+            carRigidbody.AddForce(appliedHoverForce, ForceMode.Acceleration);
+        }
+        carRigidbody.AddRelativeForce(0f, 0f, powerInput * speed);
+        carRigidbody.AddRelativeTorque(0f, turnInput * turnSpeed, 0f);
+    }
+}
---
+++ New Unity Project/Assets/readme.txt
@@ -0,0 +1,1 @@
+please give credit to alec pike when used
